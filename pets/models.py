2719692d1198bfbb9cb732ffda90ff8853053406
Artur Comoli <91695244+arturcomoli@users.noreply.github.com>
2022-07-14 17:49:10 -0300

    Revert "Woops"

--- pets/models.py
+++ pets/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-
-class Pet(models.Model):
-    name = models.CharField(max_length=100)
-    type = models.CharField(max_length=30)
-    breed = models.CharField(max_length=30)
-    birthday = models.DateField()
-    is_alive = models.BooleanField(default=True)
-    owner = models.ForeignKey(
-        "owners.Owner", on_delete=models.DO_NOTHING, related_name="pets"
-    )
+# Create your models here.
