9a273c14507d829411b9f983264abe4bc4986437
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-07-14 15:13:04 -0300

    tests: owner model tests

---
+++ owners/tests/mock.py
@@ -0,0 +1,29 @@
+staff_manager = {
+    "username": "Manager Test",
+    "first_name": "Staff",
+    "last_name": "Test",
+    "is_manager": True,
+    "is_doctor": False,
+}
+
+staff_doctor = {
+    "username": "Doctor Test",
+    "first_name": "Staff",
+    "last_name": "Test",
+    "is_manager": False,
+    "is_doctor": True,
+}
+
+owner_1 = {
+    "name": "Owner Test",
+    "email": "owner_test@mail.com",
+    "address": "Address",
+    "phone_number": 999999999,
+}
+
+owner_2 = {
+    "name": "Owner Test 2",
+    "email": "owner_test2@mail.com",
+    "address": "Address",
+    "phone_number": 999999999,
+}
