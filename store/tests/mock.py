e30572f09b3152988a58241c0ba5193445f8db04
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-07-15 00:36:26 -0300

    test: tests store model and choices

---
+++ store/tests/mock.py
@@ -0,0 +1,29 @@
+store_1 = {
+    "name": "Store Test",
+    "address": "Address 1",
+    "city": "Cidade Teste",
+    "state": "SP",
+    "is_active": False,
+}
+
+store_2 = {
+    "name": "Store Test 2",
+    "address": "Address 1",
+    "city": "Cidade Teste",
+    "is_active": False,
+}
+
+store_3 = {
+    "name": "Store Test 3",
+    "address": "Address 1",
+    "city": "Cidade Teste",
+    "state": "AL",
+}
+
+store_4 = {
+    "name": "Store Test 4",
+    "address": "Address 1",
+    "city": "Cidade Teste",
+    "state": "AB",
+    "is_active": False,
+}
