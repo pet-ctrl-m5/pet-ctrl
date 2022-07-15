2dfa3c08b0ba4de5295d718151536ca3b9b0cf96
Artur Comoli <artur.comoli@gmail.com>
2022-07-15 14:39:05 -0300

    feat: make permission for services CRUD

--- permissions/permissions.py
+++ permissions/permissions.py
@@ -145,3 +145,20 @@ class ReportsCRUDPermission(permissions.BasePermission):
             return False
 
         return True
+
+
+class ServicesCRUDPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if request.user.is_superuser:
+            return True
+
+        if not (request.user.is_superuser or request.user.is_manager):
+            return False
+
+        return True
--- services/views.py
+++ services/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from permissions.permissions import ServicesCRUDPermission
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 
 from .models import Service
@@ -6,12 +8,20 @@ from .serializers import ServiceSerializer
 
 
 class ListCreateServiceView(ListCreateAPIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [ServicesCRUDPermission]
+
     # queryset = Service.objects.all()
     queryset = Service.objects.filter(is_active__exact=True)
     serializer_class = ServiceSerializer
 
 
 class ServiceDetailsView(RetrieveUpdateAPIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [ServicesCRUDPermission]
+
     queryset = Service.objects.filter(is_active__exact=True)
     serializer_class = ServiceSerializer
     lookup_url_kwarg = "service_id"
