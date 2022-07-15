5cc2d803bc4a203037b24a2a8994d64d55465183
Artur Comoli <artur.comoli@gmail.com>
2022-07-15 20:14:14 -0300

    feat: test models for reports app

---
+++ reports/tests/mocks.py
@@ -0,0 +1,17 @@
+pet_data = {
+    "name": "Pet",
+    "type": "Test",
+    "breed": "Test Breed",
+    "birthday": "1996-08-28",
+    "is_alive": True,
+}
+
+pet_data_2 = {
+    "name": "Pet 2",
+    "type": "Test 2",
+    "breed": "Test Breed 2",
+    "birthday": "1996-08-28",
+    "is_alive": True,
+}
+
+report_data = {"report": "Test Report"}
